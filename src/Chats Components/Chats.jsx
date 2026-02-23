49159c186379918a17f657bda7ae76c0b06f54cd
ChoforJr <choforforsakang@gmail.com>
2026-02-23 04:03:46 +0100

    Worked on building and styling group chats
    User can see all the groups they are member of and by clicking on
    them the user can see all the messages in those groups, the user
    can also send messages to the group, then styling was done to made
    it all look modern

--- src/Chats Components/Chats.jsx
+++ src/Chats Components/Chats.jsx
@@ -176,47 +176,60 @@ export const PeopleChats = () => {
                   </span>
                 </div>
                 <div className={styles.messagesContainer}>
-                  {contactMessages
-                    .filter((msg) => msg.toUserId === currentContact.id)
-                    .map((msg) => (
-                      <div
-                        key={msg.keyID}
-                        className={`${styles.message} ${
-                          msg.authorId === account?.id
-                            ? styles.userMessage
-                            : styles.contactMessage
-                        }`}
-                      >
-                        {msg.content ? (
-                          <div className={styles.messageText}>
-                            <p>{msg.content}</p>
-                            <span>
-                              {new Date(msg.createdAt).toLocaleString()}
-                            </span>
+                  {contactMessages && contactMessages.length > 0
+                    ? contactMessages
+                        .filter(
+                          (msg) =>
+                            msg.toUserId === currentContact.id ||
+                            msg.authorId === currentContact.id,
+                        )
+                        .sort(
+                          (a, b) =>
+                            new Date(a.createdAt) - new Date(b.createdAt),
+                        )
+                        .map((msg) => (
+                          <div
+                            key={msg.keyID}
+                            className={`${styles.message} ${
+                              msg.authorId === account?.id
+                                ? styles.userMessage
+                                : styles.contactMessage
+                            }`}
+                          >
+                            {msg.content ? (
+                              <div className={styles.messageText}>
+                                <p>{msg.content}</p>
+                                <span>
+                                  {new Date(msg.createdAt).toLocaleString()}
+                                </span>
+                              </div>
+                            ) : (
+                              msg.files.map((file) => (
+                                <div
+                                  key={file.keyID}
+                                  className={styles.chatFile}
+                                >
+                                  <button
+                                    type="button"
+                                    className={styles.downloadBtn}
+                                    onClick={() => {
+                                      downloadFileFromUrl(
+                                        file.photo,
+                                        file.originalName,
+                                      );
+                                    }}
+                                  >
+                                    {file.originalName} <Download />
+                                  </button>
+                                  <span>
+                                    {new Date(msg.createdAt).toLocaleString()}
+                                  </span>
+                                </div>
+                              ))
+                            )}
                           </div>
-                        ) : (
-                          msg.files.map((file) => (
-                            <div key={file.keyID} className={styles.chatFile}>
-                              <button
-                                type="button"
-                                className={styles.downloadBtn}
-                                onClick={() => {
-                                  downloadFileFromUrl(
-                                    file.photo,
-                                    file.originalName,
-                                  );
-                                }}
-                              >
-                                {file.originalName} <Download />
-                              </button>
-                              <span>
-                                {new Date(msg.createdAt).toLocaleString()}
-                              </span>
-                            </div>
-                          ))
-                        )}
-                      </div>
-                    ))}
+                        ))
+                    : null}
                 </div>
                 <div className={styles.sendMessageSection}>
                   {messageType === "text" ? (
@@ -279,14 +292,307 @@ export const PeopleChats = () => {
 };
 
 export const GroupChats = () => {
-  const { auth } = useContext(ItemContext);
+  const {
+    auth,
+    account,
+    allProfiles,
+    memberGroups,
+    refreshMemberGroups,
+    refreshExploreGroups,
+    groupMessages,
+    refreshRecentGroupMessages,
+  } = useContext(ItemContext);
+  const [currentGroup, setCurrentGroup] = useState(null);
+  const [messageType, setMessageType] = useState("text");
+  const [messageText, setMessageText] = useState("");
+  const authToken = localStorage.getItem("authorization");
+
+  const handleLeaveGroup = async (e, contactID) => {
+    e.stopPropagation();
+    try {
+      const response = await fetch(`${apiUrl}/group/leave/${contactID}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: authToken,
+        },
+        body: JSON.stringify({ contactId: contactID }),
+      });
+      if (response.ok) {
+        refreshMemberGroups();
+        refreshExploreGroups();
+        return true;
+      } else {
+        const result = await response.json();
+        alert(result.errors?.[0]?.msg || result.error || "Connection failed");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while trying to connect");
+    }
+    return false;
+  };
+
+  function handleGroupClick(e, contact) {
+    e.stopPropagation();
+    setCurrentGroup(contact);
+    refreshRecentGroupMessages();
+  }
+  function clearMessages(e) {
+    e.stopPropagation();
+    setCurrentGroup(null);
+  }
+
+  const SendImages = async (e) => {
+    const files = e.target.files;
+    if (!files || files.length === 0) return;
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      formData.append("uploads", files[i]);
+    }
+    try {
+      const response = await fetch(
+        `${apiUrl}/message/image/chat/toGroup/${currentGroup.id}`,
+        {
+          method: "POST",
+          headers: { authorization: authToken },
+          body: formData,
+        },
+      );
+
+      if (response.ok) {
+        refreshRecentGroupMessages();
+      } else {
+        const contentType = response.headers.get("content-type");
+        if (contentType && contentType.includes("application/json")) {
+          const err = await response.json();
+          alert(err.error || "Upload failed");
+        } else {
+          alert("Upload failed: Server error");
+        }
+      }
+    } catch (err) {
+      console.error("Upload Error:", err);
+    }
+  };
+
+  const SendText = async () => {
+    try {
+      const response = await fetch(`${apiUrl}/message/text/toGroup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: authToken,
+        },
+        body: JSON.stringify({
+          content: messageText,
+          toGroupID: currentGroup.id,
+        }),
+      });
+
+      if (response.ok) {
+        refreshRecentGroupMessages();
+        setMessageText("");
+      } else {
+        const err = await response.json();
+        alert(err.error || "Server error: Message not sent");
+      }
+    } catch (err) {
+      console.error("Upload Error:", err);
+    }
+  };
+
+  async function downloadFileFromUrl(url, filename) {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const blob = await response.blob();
+
+      const blobUrl = URL.createObjectURL(blob);
+
+      const link = document.createElement("a");
+      link.href = blobUrl;
+      link.download = filename;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      URL.revokeObjectURL(blobUrl);
+    } catch (error) {
+      console.error("Error while downloading the file:", error);
+    }
+  }
 
   return (
     <div className={styles.chatBody}>
       {auth ? (
         <>
-          <section className={styles.groups}></section>
-          <section className={styles.messages}></section>
+          <section className={styles.groups}>
+            {memberGroups.map((group) => (
+              <div
+                key={group.keyID}
+                className={styles.contact}
+                onClick={(e) => handleGroupClick(e, group)}
+              >
+                <img src={group.profilePhoto} alt={group.name} />
+                <div>
+                  <p>{group.name}</p>
+                  <button onClick={(e) => handleLeaveGroup(e, group.id)}>
+                    Leave Group <UserMinus />
+                  </button>
+                </div>
+              </div>
+            ))}
+          </section>
+          <section className={styles.messages}>
+            <button>Create a Group</button>
+            {currentGroup ? (
+              <>
+                <div
+                  className={styles.messagesHeader}
+                  onClick={(e) => clearMessages(e)}
+                >
+                  <img
+                    src={currentGroup.profilePhoto}
+                    alt={currentGroup.name}
+                  />
+                  <span>
+                    Messages with {currentGroup.name} (click to clear)
+                  </span>
+                </div>
+                <div className={styles.messagesContainer}>
+                  {groupMessages && groupMessages.length > 0
+                    ? groupMessages
+                        .filter((msg) => msg.toGroupId === currentGroup.id)
+                        .map((msg) => (
+                          <div
+                            key={msg.keyID}
+                            className={`${styles.message} ${
+                              msg.authorId === account?.id
+                                ? styles.userMessage
+                                : styles.contactMessage
+                            }`}
+                          >
+                            {msg.content ? (
+                              <div className={styles.messageText}>
+                                <div className={styles.authorInfo}>
+                                  <img
+                                    src={
+                                      allProfiles.filter(
+                                        (profile) =>
+                                          profile.id === msg.authorId,
+                                      )[0]?.photo || "/default avatar.png"
+                                    }
+                                    alt="Author"
+                                    className={styles.authorPhoto}
+                                  />
+                                  <span className={styles.authorName}>
+                                    {allProfiles.filter(
+                                      (profile) => profile.id === msg.authorId,
+                                    )[0]?.displayName || "Unknown User"}
+                                  </span>
+                                </div>
+                                <p>{msg.content}</p>
+                                <span className={styles.messageTime}>
+                                  {new Date(msg.createdAt).toLocaleString()}
+                                </span>
+                              </div>
+                            ) : (
+                              msg.files.map((file) => (
+                                <div
+                                  key={file.keyID}
+                                  className={styles.chatFile}
+                                >
+                                  <div className={styles.authorInfo}>
+                                    <img
+                                      src={
+                                        allProfiles.filter(
+                                          (profile) =>
+                                            profile.id === msg.authorId,
+                                        )[0]?.photo || "/default avatar.png"
+                                      }
+                                      alt="Author"
+                                      className={styles.authorPhoto}
+                                    />
+                                    <span className={styles.authorName}>
+                                      {allProfiles.filter(
+                                        (profile) =>
+                                          profile.id === msg.authorId,
+                                      )[0]?.displayName || "Unknown User"}
+                                    </span>
+                                  </div>
+                                  <button
+                                    type="button"
+                                    className={styles.downloadBtn}
+                                    onClick={() => {
+                                      downloadFileFromUrl(
+                                        file.photo,
+                                        file.originalName,
+                                      );
+                                    }}
+                                  >
+                                    {file.originalName} <Download />
+                                  </button>
+                                  <span className={styles.messageTime}>
+                                    {new Date(msg.createdAt).toLocaleString()}
+                                  </span>
+                                </div>
+                              ))
+                            )}
+                          </div>
+                        ))
+                    : null}
+                </div>
+                <div className={styles.sendMessageSection}>
+                  {messageType === "text" ? (
+                    <>
+                      <button onClick={() => setMessageType("image")}>
+                        Send Images instead
+                      </button>
+                      <textarea
+                        value={messageText}
+                        onChange={(e) => setMessageText(e.target.value)}
+                        placeholder="Type your message..."
+                      />
+                      <button type="button" onClick={SendText}>
+                        Send
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <button onClick={() => setMessageType("text")}>
+                        Send Text instead
+                      </button>
+                      <p>Remeber that only jpg and png files are allowed.</p>
+                      <p>A File must be less than 1MB.</p>
+                      <p>No more than 5 files can be uploaded at onces.</p>
+                      <label className={styles.uploadBtn}>
+                        Send images
+                        <input
+                          type="file"
+                          hidden
+                          onChange={SendImages}
+                          accept="image/png, image/jpeg"
+                          multiple
+                        />
+                      </label>
+                    </>
+                  )}
+                </div>
+              </>
+            ) : (
+              <>
+                <h1>Select a Group to view messages</h1>
+                <img
+                  src="/group conversation.svg"
+                  alt="group conversation"
+                  className={styles.conversationImage}
+                />
+              </>
+            )}
+          </section>
         </>
       ) : (
         <h1>
